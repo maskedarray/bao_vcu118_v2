6107c2294d601d39232b82bf39ed7349bcccd148
maskedarray <rao.arrn@gmail.com>
2023-08-07 17:22:12 -0400

    config files not tracked. Made error when removed pmu from config.c

---
+++ bao-hypervisor/configs/alsaqr-linux/config.c
@@ -0,0 +1,57 @@
+#include <config.h>
+
+#include <config.h>
+
+VM_IMAGE(linux_image, XSTR(/home/ubuntu_20/BAO-VCU-118_v2/bao-baremetal-guest/build/cva6/baremetal.bin));
+
+struct config config = {
+    
+    CONFIG_HEADER
+
+    .vmlist_size = 1,
+    .vmlist = {
+        { 
+            .image = {
+                .base_addr = 0x80200000,
+                .load_addr = VM_IMAGE_OFFSET(linux_image),
+                .size = VM_IMAGE_SIZE(linux_image)
+            },
+
+            .entry = 0x80200000,
+
+            .platform = {
+                .cpu_num = 1,
+                
+                .region_num = 1,
+                .regions =  (struct mem_region[]) {
+                    {
+                        .base = 0x80200000,
+                        .size = 0x10E00000,
+                    }
+                },
+
+                .dev_num = 2,
+                .devs =  (struct dev_region[]) {
+                    {
+                        .pa = 0x40000000,   
+                        .va = 0x40000000,  
+                        .size = 0x10000,  
+                        .interrupt_num = 2,
+                        .interrupts = (uint64_t[]) {2}
+                    },
+                    {
+                        .pa = 0x18000000,   
+                        .va = 0x18000000,  
+                        .size = 0x00001000,  
+                        .interrupt_num = 4,
+                        .interrupts = (uint64_t[]) {4,5,6,7}
+                    },
+                },
+
+                .arch = {
+                   .plic_base = 0xc000000,
+                }
+            },
+        }
+     }
+};
